459d9f3649001c45167020510e1cc4052d0b26e4
Алексеев Юрий <71309015+AlekseevYuri@users.noreply.github.com>
2020-11-17 00:47:01 +0300

    Update zvezdochka.py

--- zvezdochka.py
+++ zvezdochka.py
@@ -1,17 +1,23 @@
 import turtle as tl
 def draw_fractal(scale):
-    if scale >= 1:
+    if scale >= 25:
         draw_fractal(scale/3)
         tl.left(60)
         draw_fractal(scale/3)
         tl.right(120)
         draw_fractal(scale/3)
         tl.left(60)
-        draw_fractal(scale/3)
+        draw_fractal(scale/3)   
     else:
         tl.forward(scale)
-scale = 200
+scale = 500
 tl.pensize(2)
+tl.penup()
+tl.forward(-200)
+tl.left(90)
+tl.forward(110)
+tl.right(90)
+tl.pendown()
 draw_fractal(scale)
 tl.right(120)
 draw_fractal(scale)
