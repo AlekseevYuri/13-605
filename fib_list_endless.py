01b45dcd6dd22fde85c5df13cf8c185748ff52e7
Алексеев Юрий <71309015+AlekseevYuri@users.noreply.github.com>
2020-12-06 23:10:17 +0300

    Add files via upload

--- fib_list_endless.py
+++ fib_list_endless.py
@@ -2,13 +2,13 @@ import numpy
 import itertools
 spisok  = [1,1]
 class el:    
-    """По объектам этого класса можно итерироваться и получать 6 чисел Фибоначчи"""
+  
     
     class _el_iter:
-        """Внутренний класс — итератор"""
+
         def __init__(self):
             self.i = 0
-            self.els = spisok # они у нас выше были
+            self.els = spisok
         def __next__(self):
             if self.i>=n:
                 print("DONE!")
@@ -25,9 +25,8 @@ class el:
                     return (str(self.i)+") "+str(self.els[j]))
 #Используется тот же код, что и в блокноте на сайте.            
     def __iter__(self):
-        """Создать и вернуть итератор"""
         return el._el_iter()
-n = 10
+n = 300 #Количество чисел.
 e = el()
 for w in e:
     print( w)
