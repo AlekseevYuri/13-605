78133420b9310a3f07d62778a01e47c2639079b6
Алексеев Юрий <71309015+AlekseevYuri@users.noreply.github.com>
2020-12-09 12:17:37 +0300

    Add files via upload

--- fib_list_endless.py
+++ fib_list_endless.py
@@ -10,24 +10,24 @@ class el:
             self.i = 0
             self.els = spisok
         def __next__(self):
-            if self.i>=n:
-                print("DONE!")
-                raise StopIteration()
-            else:
-                if self.i > 10:#Если список кончился, он продлевается при итерации.
-                    self.els +=[spisok[-1]+spisok[-2]]
-                    j = self.i
-                    self.i += 1
-                    return (str(self.i)+") "+str(self.els[j]))
-                else:
-                    j = self.i
-                    self.i += 1
-                    return (str(self.i)+") "+str(self.els[j]))
+            if self.i > 10:#Если список кончился, он продлевается при итерации.
+                self.els +=[spisok[-1]+spisok[-2]]
+                j = self.i
+                self.i += 1
+                return (str(self.i)+") "+str(self.els[j]))
+            else:#eсли не кончился, двигаемся по нему
+                j = self.i
+                self.i += 1
+                return (str(self.i)+") "+str(self.els[j]))
 #Используется тот же код, что и в блокноте на сайте.            
     def __iter__(self):
         return el._el_iter()
-n = 300 #Количество чисел.
-e = el()
-for w in e:
-    print( w)
+n = 30 #Количество чисел.
+f6 = el()
+
+for i, f in zip(
+    itertools.count(1),
+    itertools.islice(f6, n)
+):
+    print(f)
 
